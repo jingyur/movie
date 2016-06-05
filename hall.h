c60f2151c024953ca100bf5bda754a727da4ecff
jing <jingyurlex@icloud.com>
2016-06-05 11:57:49 +0800

    add preorder

--- hall.h
+++ hall.h
@@ -6,6 +6,7 @@
 typedef struct {
 	int row;
  	int col;
+	bool preo;
  	bool occupy;
 } seat;
 
@@ -14,8 +15,11 @@ typedef struct {
 } hall;      	
 
 hall* init_hall();
+hall* random_hall_occp(hall* h);
 
 void seat_print(const seat* s);
 
 bool hall_occp(hall* h, int row, int col);
+bool hall_preo(hall* h, int row, int col);
+bool hall_preo_cancel(hall* h, int row, int col);
 void hall_print(const hall* h);
--- main.c
+++ main.c
@@ -1,32 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 #include "utility.h"
 #include "movie.h"
 #include "hall.h"
 
-hall* random_hall_occp(hall* h)
-{
-    srand(time(NULL));
-
-    for(int i = 0; i < MAX_ROW; ++i) {
-        for(int j = 0; j < MAX_COL; ++j) {
-            if(rand() % 100 > 80) {
-                hall_occp(h, i, j);
-            }
-        }
-    }
-    return h;
-}
-
 int main()
 {
 	debug("start movie...");
-
-	hall* h = init_hall();
-    random_hall_occp(h);
-	hall_print(h);
 
 	debug("end movie...");
 
